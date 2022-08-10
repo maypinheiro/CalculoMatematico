e817463595f3550525fb7cc227b85db36b7ec86e
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-10 08:19:15 -0300

    Separação de Contexto

--- Calculos.cs
+++ Calculos.cs
@@ -7,19 +7,26 @@ namespace CalculadoraDeValores
     class Calculos
     {
  
-    
-        public  float CalcularIMC(float peso, float altura)
+        public  String CalcularIMC(float peso, float altura)
         {
                 float imc = peso / (altura * altura);
-                return imc;
-        }
+                string resultado =" ";
+ 
+            if (imc < 18.5) resultado =" A baixo do peso.";
+                else if (imc < 24.9) resultado = "Peso Normal.";
+                else if (imc < 29.9) resultado = " Excesso de peso.";
+                else if (imc < 35) resultado =   " Obesidade.";
+                else resultado = "Obesidade extrema.";
 
+            return resultado;
+           
+
+        }
         public  double CalculaArea( double raioDoCirculo)
         {
                  double areaCirculo = Math.PI * Math.Pow(raioDoCirculo, 2);
                 return areaCirculo; 
         }
-
         public  float CalculaRegraDeTres(float a, float b, float c)
         {
                 float d = (c * b) / a;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace CalculadoraDeValores
 {
@@ -7,12 +8,10 @@ namespace CalculadoraDeValores
         static void Main(string[] args)
         {
             MenuPrincipal();
+            SelecionaOperacao();
         }
         static void MenuPrincipal()
         {
-            Calculos calculo = new Calculos();
-            ValidaOperadores validaOperadores = new ValidaOperadores();
-
             Console.Clear();
             Console.WriteLine("Escolha um Opção");
             Console.WriteLine("1- Calculadora IMC");
@@ -20,50 +19,34 @@ namespace CalculadoraDeValores
             Console.WriteLine("3- Calculadora regra de 3");
             Console.WriteLine("4- Sair");
             Console.WriteLine("------------------------------");
-            Console.WriteLine("Selecione uma Opção");
+        }
 
+        private static void SelecionaOperacao()
+        {
+            Calculos calculo = new Calculos();
+            ValidaOperadores validarOperadores = new ValidaOperadores();
 
-            int op = int.Parse(Console.ReadLine()) -1;
+            int op = int.Parse(Console.ReadLine()) - 1;
 
             switch (op)
             {
-
-                case (int)Calcular.IMC: {
-                        Console.Write("Diguite seu peso (Kg):  ");
+                case (int)Calcular.IMC:
+                    {
+                        Console.WriteLine("Diguite seu peso(KG)");
                         float peso = float.Parse(Console.ReadLine());
-                        Console.Write("Diguite sua altura (M):  ");
+                        Console.WriteLine("Diguite sua altura(M)");
                         float altura = float.Parse(Console.ReadLine());
-                        float imc;
-                        string texto;
-
-                        if (validaOperadores.IMC(peso, altura)) {
-                            imc = calculo.CalcularIMC(peso, altura);
-                                if (imc < 18.5)      texto= "A baixo do peso.";
-                                else if (imc < 24.9) texto= "Peso Normal.";
-                                else if (imc < 29.9) texto = "Excesso de peso.";
-                                else if (imc < 35)   texto = "Obesidade.";
-                                else texto = "Obesidade extrema.";
-                            Console.WriteLine(texto + " IMC: " + imc.ToString());
-                            Console.ReadKey();
-                            MenuPrincipal();
-                        }
-                        else
-                        {
-                            Console.ReadKey();
-                            MenuPrincipal();
-                        }
+                        if (validarOperadores.IMC(peso, altura))
+                           Console.WriteLine(calculo.CalcularIMC(peso, altura));
                     };
                     break;
-
+                    
                 case (int)Calcular.Area: {
                         Console.Write(" Informe o raio do Círculo: ");
-
                         double raioDoCirculo = double.Parse(Console.ReadLine());
-                        if (validaOperadores.Area(raioDoCirculo))
-                            Console.WriteLine(calculo.CalculaArea(raioDoCirculo));
 
-                        Console.ReadKey();
-                        MenuPrincipal();
+                        if (validarOperadores.Area(raioDoCirculo))
+                        Console.WriteLine($"Área do circulo : {calculo.CalculaArea(raioDoCirculo).ToString("N2")}");
                     };
                     break;
 
@@ -75,20 +58,22 @@ namespace CalculadoraDeValores
                         float b = float.Parse(Console.ReadLine());
                         Console.Write("Insira o valor de C: ");
                         float c = float.Parse(Console.ReadLine());
-                        Console.WriteLine(calculo.CalculaRegraDeTres(a,b,c));
-                        Console.ReadKey();
-                        MenuPrincipal();
-
+                        Console.WriteLine($" Valor de X:{calculo.CalculaRegraDeTres(a,b,c)}");
                     };
                     break;
 
-                case (3): Console.WriteLine("Programa FINALIZADO COM SUCESSO");
+                case (3): Console.WriteLine(" Programa FINALIZADO COM SUCESSO");
                     Console.ReadKey();
                     System.Environment.Exit(0); break;
+
                 default: MenuPrincipal(); break;
             }
+
+            Console.ReadKey();
+            MenuPrincipal();
+            SelecionaOperacao();
+
         }
-
-
+        
     }
 }
